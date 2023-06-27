e381387944a10d68d791a4df7d38ff1f224769de
Britney Giles <Blgiles04@gmail.com>
2023-06-27 10:45:10 -0600

    Initial Commit

---
+++ Remind/RemindApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindApp.swift
+//  Remind
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
